9f2090b21c44a8f25cfe32f6d36655113f9473df
safin-calling <safin248@outlook.com>
2025-03-04 13:25:31 +0600

    Update environment variables and adjust dashboard layout for media display

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "143.110.241.146",
-    port: "5002",
+    host: "10.0.60.34",
+    port: "3005",
   },
-})
+});
